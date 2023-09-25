2b102c1ac6b2a91bf4c957a09f3197e2687d851d
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2023-09-26 00:19:03 +0900

    環境構築

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module example.com/m/v2
 
 go 1.19
 
-require github.com/labstack/echo/v4 v4.11.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/labstack/echo/v4 v4.11.1
+)
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -14,4 +19,5 @@ require (
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+var db *sqlx.DB
+
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-		e.POST("/login", PostLogin)
-		e.GET("/note", GetNote)
-		e.GET("/note/{:id}", GetNoteId)
-		e.POST("/note", PostNote)
-		e.GET("/note/author/{:id}", GetNoteAuthorId)
-    e.Logger.Fatal(e.Start(":3000"))
+	InitDB();
+
+  e := echo.New()
+	e.Use(middleware.Logger()) 
+
+  e.GET("/", func(c echo.Context) error {
+      return c.String(http.StatusOK, "Hello, World!")
+  })
+	e.POST("/login", PostLogin)
+	e.GET("/note", GetNote)
+	e.GET("/note/{:id}", GetNoteId)
+	e.POST("/note", PostNote)
+	e.GET("/note/author/{:id}", GetNoteAuthorId)
+  e.Logger.Fatal(e.Start(":3000"))
+}
+
+func InitDB() {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := mysql.Config{
+		User:      os.Getenv("MYSQL_USER"),
+		Passwd:    os.Getenv("MYSQL_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("MYSQL_HOSTNAME") + ":" + os.Getenv("MYSQL_PORT"),
+		DBName:    os.Getenv("MYSQL_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	db, err = sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func PostLogin(c echo.Context) error {
