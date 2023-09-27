40c11f3720f6e216dc6188907819859dec2f524a
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2023-09-27 12:34:42 +0900

    CORSの設定（本来はサイトを限定すべきだ）

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 
         e := echo.New()
         e.Use(middleware.Logger())
+        e.Use(middleware.CORS())
 
         e.GET("/", func(c echo.Context) error {
                 return c.String(http.StatusOK, "Hello, World!")
